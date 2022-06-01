876c3e699b33f523724efd6fa8e6bc6f2a9802d8
drjackherer <mongkolpersonal@gmail.com>
2022-06-01 11:35:57 -0700

    ethods finished

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodDemo
+{
+     class Program
+    {
+        //definition
+       
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        } 
+        static void Main(string[] args)
+        {
+            //Method call
+            Console.WriteLine (Program.Add(4,8));
+        }
+        public static void subtract(int x, int  y)
+        {
+            var answer = x- y;
+            Console.Write(answer);
+        }
+    }
+}
